7b16ae8aa33862767d4a70a7abaea83b09fc0768
mastera7 <77801257+mastera7@users.noreply.github.com>
2021-05-23 06:37:14 +0600

    Add files via upload

--- Main1/src/main/java/Main.java
+++ Main1/src/main/java/Main.java
@@ -2,16 +2,15 @@ import java.io.*;
 import java.lang.*;
 import java.util.Scanner;
 
-
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello again! Tasks are: 1)MergeSort; 2)Quick Sort 3)HashTable");
-        System.out.println("Choose the task from 1 to 3:");
+        System.out.println("Hello again! Tasks are: 1)MergeSort; 2)Quick Sort 3)BST 4)HashTable");
+        System.out.println("Choose the task from 1 to 4:");
         int choose;
-        while(true) {
+        while (true) {
             choose = scan.nextInt();
-            switch(choose) {
+            switch (choose) {
                 case 1:
                     System.out.println(MS());
                     break;
@@ -19,6 +18,9 @@ public class Main {
                     System.out.println(QS());
                     break;
                 case 3:
+                    System.out.println(new BST());
+                    break;
+                case 4:
                     System.out.println("Oops! Didn't finished it (");
                 default:
                     System.out.println("Exiting from program....");
@@ -28,14 +30,14 @@ public class Main {
 
     }
 
-    static String MS(){
-        Scanner scan=new Scanner(System.in);
-        int n= scan.nextInt();
-        String[] sarray=new String[n];
-        for(int i=0;i<n;i++){
-            sarray[i]=scan.next();
+    static String MS() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] sarray = new String[n];
+        for (int i = 0; i < n; i++) {
+            sarray[i] = scan.next();
         }
-        MergeSort<String> stringSorter   = new MergeSort<>();
+        MergeSort<String> stringSorter = new MergeSort<>();
         stringSorter.mSort(sarray, 0, sarray.length - 1);
         System.out.println(java.util.Arrays.toString(sarray));
         return "Here we go again! Choose the next task 1 or 3:";
@@ -46,14 +48,51 @@ public class Main {
 
         String[] arrayOfStrings = {"Eminem", "Jay-Z", "Rihanna", "Britney", "JustinTimberlake", "TaylorSwift", "SlimShady", "BananaGuy", "Moriarti", "Jlo", "BonneyM", "Putin", "Oxxy", "PapaRimskiy", "Rasputin"};
 
-        QuickSort<String> stringSorter   = new QuickSort<>();
+        QuickSort<String> stringSorter = new QuickSort<>();
         stringSorter.qSort(arrayOfStrings, 0, arrayOfStrings.length - 1);
         System.out.println(java.util.Arrays.toString(arrayOfStrings));
 
 
         return "Here we go again! Choose the next task 1 or 3:";
     }
+
+    public static class BST< T extends Comparable<T>> {
+        Node root;
+        class Node {
+            final ThreadLocal<T> data = new ThreadLocal<T>();
+            Node left;
+            Node right;
+
+            public Node(T data) {
+                this.data.set(data);
+            }
+        }
+
+        public boolean isempty() {
+            return root == null;
+        }
+
+        public void insert(T val) {
+            if(isempty())
+                root = new Node(val);
+            insert(root, val);
+        }
+
+        private void insert(Node node, T val) {
+
+            if(val.compareTo((T) node.data) < 0) {
+                if (node.left != null) {
+                } else {
+                    node.left = new Node(val);
+                }
+                insert(node.left, val);
+            }
+            if(node.right == null)
+                node.right = new Node(val);
+            insert(node.right, val);
+        }
+    }
+
+
 }
-
-
 
--- Main1/src/main/java/MergeSort.java
+++ Main1/src/main/java/MergeSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MergeSort<T extends Comparable<? super T>> {
 
     void merge(T[] arr, int start, int mid, int end)
@@ -5,29 +7,25 @@ public class MergeSort<T extends Comparable<? super T>> {
         T[] arrLeft  = (T[]) new Comparable[mid - start + 1];
         T[] arrRight = (T[]) new Comparable[end - mid];
 
-        for (int i = 0; i < arrLeft.length; ++i)
-            arrLeft[i] = arr[start + i];
+        Arrays.setAll(arrLeft, i -> arr[start + i]);
 
-        for (int i = 0; i < arrRight.length; ++i)
-            arrRight[i] = arr[mid + 1 + i];
+        Arrays.setAll(arrRight, i -> arr[mid + 1 + i]);
 
         int leftIndex = 0, rightIndex = 0;
 
         int IndCurrent = start;
 
-        while (leftIndex < arrLeft.length && rightIndex < arrRight.length)
-        {
-            if (arrLeft[leftIndex].compareTo(arrRight[rightIndex]) <= 0)
-            {
-                arr[IndCurrent] = arrLeft[leftIndex];
-                leftIndex++;
-            }
-            else
-            {
-                arr[IndCurrent] = arrRight[rightIndex];
-                rightIndex++;
-            }
-            IndCurrent++;
+        if (leftIndex < arrLeft.length && rightIndex < arrRight.length) {
+            do {
+                if (arrLeft[leftIndex].compareTo(arrRight[rightIndex]) <= 0) {
+                    arr[IndCurrent] = arrLeft[leftIndex];
+                    leftIndex++;
+                } else {
+                    arr[IndCurrent] = arrRight[rightIndex];
+                    rightIndex++;
+                }
+                IndCurrent++;
+            } while (leftIndex < arrLeft.length && rightIndex < arrRight.length);
         }
 
         while (leftIndex < arrLeft.length) arr[IndCurrent++] = arrLeft[leftIndex++];
@@ -37,14 +35,14 @@ public class MergeSort<T extends Comparable<? super T>> {
 
     void mSort(T[] arr, int start, int end)
     {
-        if (start < end)
-        {
-            int mid = (start + end) / 2;
-
-            mSort(arr, start, mid);
-            mSort(arr, mid + 1, end);
-
-            merge(arr, start, mid, end);
+        if (start >= end) {
+            return;
         }
+        int mid = (start + end) / 2;
+
+        mSort(arr, start, mid);
+        mSort(arr, mid + 1, end);
+
+        merge(arr, start, mid, end);
     }
 }
